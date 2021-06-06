b5764a397e06e5eedcc81fd26438a28330050be1
Baturay Caglasin <baturaycaglasin02@gmail.com>
2021-06-06 15:27:53 +0300

    final

--- src/StudentDataBaseApplication/Student.java
+++ src/StudentDataBaseApplication/Student.java
@@ -1,10 +1,12 @@
 package StudentDataBaseApplication;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Student {
     private String studentID;
-    private static int tuition = 0;
+    private static int balancetuition = 0;
     private static int coursesTuition = 600;
     private static int id = 1000;
     private String firstName;
@@ -40,9 +42,9 @@ do {
     System.out.println("Please note that press Q button for quit the system.");
     Scanner scanner = new Scanner(System.in);
     String course = scanner.nextLine();
-    if(!course.equals("Q")){
-courses=courses + " " + course;
-tuition = tuition + coursesTuition;
+    if(!course.contentEquals("Q" )){
+    courses=courses + " " + course;
+    balancetuition = balancetuition + coursesTuition;
     }
     else{
         System.out.println("The system should be quit!");
@@ -50,12 +52,34 @@ tuition = tuition + coursesTuition;
     }
 }while (true);
         System.out.println("Enrolled in:" + " "+ courses) ;
-        System.out.println("The price will be: " + " " + tuition);
+        System.out.println("The price will be: " + " " + balancetuition);
     }
+
+
+//view balance
+
+public void viewBalance(){
+    System.out.println("Your balance is: " + balancetuition);
 }
+
+
 // Pay tuition calculate method
+public void payTuition(){ //payment: adamın kredi miktarı
+       System.out.println("Enter your payment: ");
+        Scanner scanner = new Scanner(System.in);
+        int payment = scanner.nextInt();
+        System.out.println("Your entered payment is: " + payment);
+        balancetuition = balancetuition - payment;
+        System.out.println("Thank you for your payment: " + " " + payment);
+        viewBalance();
+
+}
+
 // Show status method
+    public String showStatus(){
+        return "Student Name: " + firstName + "  " + lastName +  " " + " The courses he / she enrolled: " + " " + courses + " " + "Balance: " + " " + balancetuition;
+    }
 
 
-
+}
 
--- src/StudentDataBaseApplication/StudentDataBaseApp.java
+++ src/StudentDataBaseApplication/StudentDataBaseApp.java
@@ -1,11 +1,26 @@
 package StudentDataBaseApplication;
 
+import java.util.Scanner;
+
 public class StudentDataBaseApp {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.courses();
 //Ask how many students will be added
+        System.out.println("Enter how many students will be enrolled: ");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStudents = scanner.nextInt();
+        Student [] students = new Student[numberOfStudents - 1 ];
+
 
 // Add that number of students on the database
+        for (int i = 0; i < numberOfStudents; i++) {
+            students[i] = new Student(); //!
+            students[i].courses();
+            students[i].payTuition();
+          //  System.out.println(students[i].showStatus());
+        }
+        System.out.println(students[0].showStatus());
+        System.out.println(students[1].showStatus());
+        System.out.println(students[2].showStatus());
+
     }
 }
